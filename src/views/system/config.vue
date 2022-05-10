d7f6485a0f3d2505a99d8256817931d3caaadd70
llk <540811253@qq.com>
2022-05-10 08:55:04 +0800

    1. 添加了apifox的moc文件 2. 完善了项目页面模块

---
+++ src/views/system/config.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-container">相关前端配置参数</div>
+</template>
+
+<script>
+export default {
+  name: "config",
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script>
+
+<style>
+</style>
